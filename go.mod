9883e9103bfa0fbe7ececbb9959b6be94f1d5b12
jaserino <jaserino1@gmail.com>
2023-06-20 17:02:44 -0400

    setup server checked response using thunder client

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/jaserino/rssagg
 
 go 1.18
+
+require (
+	github.com/go-chi/chi/v5 v5.0.8
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,18 +1,35 @@
 package main
 
-import (
-	"fmt"
+import (	
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	
+	// pulls .env variables into current environment to use
+	godotenv.Load()
 
 	portString := os.Getenv("PORT")
 	if portString == ""{
 		log.Fatal("PORT is not found in the environment")
 	}
 
-	fmt.Println("Port:",portString)
+	// creating new chi router object
+	r := chi.NewRouter()
+
+	// connecting router to http server
+	srv := &http.Server {
+		Handler: r,
+		Addr: ":" + portString,
+	}
+	log.Printf("Server starting on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
